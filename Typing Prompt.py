3b13aed930f8fc1f62badbd36b4d67fc706d8d87
danielrenner44 <danielrenner44@gmail.com>
2022-06-30 19:52:58 -0700

    first commit

---
+++ Typing Prompt.py
@@ -0,0 +1,5 @@
+#Prompt for testing typing
+Typing_Prompt= [
+    "This will be a test of how fast you can type. It is important to type as many words correctly as you can." 
+    "You will be scored based upon the correct number of words typed."
+]
